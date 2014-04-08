c2fd101e39936b6f17021f2a213a27f4ee4391de
Sasha Demyanik <alexander.demyanik@gmail.com>
2014-04-08 16:05:41 -0700

    Added Tweet model

---
+++ WSUVMessageBoard/Tweet.h
@@ -0,0 +1,15 @@
+//
+//  Tweet.h
+//  WSUVMessageBoard
+//
+//  Created by Alexander Paul Demyanik on 4/8/14.
+//  Copyright (c) 2014 WSUV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tweet : NSObject <NSCoding, NSCopying>
+
+
+
+@end
---
+++ WSUVMessageBoard/Tweet.m
@@ -0,0 +1,13 @@
+//
+//  Tweet.m
+//  WSUVMessageBoard
+//
+//  Created by Alexander Paul Demyanik on 4/8/14.
+//  Copyright (c) 2014 WSUV. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+@end
